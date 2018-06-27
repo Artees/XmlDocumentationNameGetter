59228383aac917c1484b97e2ad133951ac0b49db
Artees <Artees@users.noreply.github.com>
2018-06-27 15:30:34 +0300

    Bug fix: creating a NuGet package.

--- XmlDocumentationNameGetter/PackNuGet.bat
+++ XmlDocumentationNameGetter/PackNuGet.bat
@@ -1,2 +1,3 @@
 cd /D "%~dp0"
+msbuild XmlDocumentationNameGetter.csproj /t:rebuild /verbosity:quiet /p:Configuration=Release
 nuget pack XmlDocumentationNameGetter.csproj -OutputDirectory ..\..\..\LocalNuGet -Prop Configuration=Release
